d7d784f8f83d2997534c9194e65f766481f594fa
biroveyou <biroveyou@gmail.com>
2026-07-30 18:16:59 +0000

    Add operate function to script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function add(firstNumber, secondNumber) {
 };
 
 function subtract(minuend, subtrahend) {
-    return firstNumber - secondNumber;
+    return minuend - subtrahend;
 };
 
 function multiply(firstNumber, secondNumber) {
@@ -14,10 +14,22 @@ function multiply(firstNumber, secondNumber) {
 
 function divide(dividend, divisor) {
     return dividend / divisor;
-}
+};
+
+function operate(operator, firstNum, secondNum) {
+    switch (operator) {
+        case "plus":
+            return add(firstNum, secondNum);
+        case "minus":
+            return subtract(firstNum, secondNum);
+        case "times":
+            return multiply(firstNum, secondNum);
+        case "divide":
+            return divide(firstNum, secondNum);
+    };
+};
 
 // A calculator operation will consist of a number, an operator, and another number
 let firstPositionNumber = 0;
 let secondPositionNumber = 0;
-let operator = "";
-
+let calculationOperator = "";

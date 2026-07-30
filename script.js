986cb4476934c2a89550178b31a711500bdb23a1
biroveyou <biroveyou@gmail.com>
2026-07-30 18:03:09 +0000

    Add divide function and rename subtract function parameter on script.js

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ function add(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 };
 
-function subtract(firstNumber, secondNumber) {
+function subtract(minuend, subtrahend) {
     return firstNumber - secondNumber;
 };
 
 function multiply(firstNumber, secondNumber) {
     return firstNumber * secondNumber;
 };
+
+function divide(dividend, divisor) {
+    return dividend / divisor;
+}
+
+console.log(divide(50, 8));

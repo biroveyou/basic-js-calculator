1ff27e0102612582f5de2faa92d2f241c709700a
biroveyou <biroveyou@gmail.com>
2026-08-03 16:42:38 +0000

    Add working number and operator on the display

--- script.js
+++ script.js
@@ -18,53 +18,67 @@ function divide(dividend, divisor) {
 
 function operate(operator, firstNum, secondNum) {
     switch (operator) {
-        case "plus":
+        case "+":
             return add(firstNum, secondNum);
-        case "minus":
+        case "-":
             return subtract(firstNum, secondNum);
-        case "times":
+        case "x":
             return multiply(firstNum, secondNum);
-        case "divide":
+        case "÷":
             return divide(firstNum, secondNum);
     };
 };
 
 // A calculator operation will consist of a number, an operator, and another number
-let firstNumberPosition = null;
-let secondNumberPosition = null;
-let calculationOperator = null;
+let firstNumberPosition = " ";
+let secondNumberPosition = " ";
+let calculationOperator = " ";
 let currentPosition = 1;
 
 function updateDisplay(number, position, operator) {
     const calculatorDisplay = document. querySelector(".display");
+    let outputText;
 
     if (position === 1) {
-        if (firstNumberPosition === null) {
+        if (firstNumberPosition === " ") {
             firstNumberPosition = number;
         } else {
             firstNumberPosition += number
         }
     }
     if (position === 2) {
-        if (secondNumberPosition === null) {
+        if (secondNumberPosition === " ") {
             secondNumberPosition = number;
         } else {
             secondNumberPosition += number
         }
     }
-    
-    calculationOperator = operator;
 
-    calculatorDisplay.innerText = firstNumberPosition, " ", calculationOperator, " ", secondNumberPosition; 
+    calculatorDisplay.innerText = `${firstNumberPosition} ${calculationOperator} ${secondNumberPosition}`; 
+    console.log(calculatorDisplay.innerText);
 };
 
 const numberButtons = document.querySelectorAll(".number-btn");
 for (let button of numberButtons) {
-    console.log(button);
     button.addEventListener("click", (btn) => {
         const target = event.target;
         const textNumberButton = target.innerText;
 
         updateDisplay(textNumberButton, currentPosition, calculationOperator);
+    });
+};
+
+const operatorButtons = document.querySelectorAll(".operator-btn");
+for (let button of operatorButtons) {
+    button.addEventListener("click", (btn) => {
+        const target = event.target;
+        const textOperatorButton = target.innerText;
+        
+        if (firstNumberPosition !== " ") {
+            calculationOperator = textOperatorButton;
+            updateDisplay(0, 3, calculationOperator);
+
+            currentPosition = 2;
+        }
     });
 };
